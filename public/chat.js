6cf5fe2fe5c02cc1051d392a4433af9aa8f74bb2
Maxime Aubaret <maxime@particleslab.com>
2014-01-26 20:36:34 +0100

    Change io.connect(localhost) to io.connect()

--- public/chat.js
+++ public/chat.js
@@ -1,12 +1,12 @@
 ﻿window.onload = function() {
- 
+
     var messages = [];
-    var socket = io.connect('http://localhost:5000');
+    var socket = io.connect();
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("name");
- 
+
     socket.on('message', function (data) {
         if(data.message) {
             messages.push(data);
@@ -21,7 +21,7 @@
             console.log("There is a problem:", data);
         }
     });
- 
+
     sendButton.onclick = sendMessage = function() {
         if(name.value == "") {
             alert("Please type your name!");
@@ -31,7 +31,7 @@
 			field.value = "";
         }
     };
-	
+
 	$(document).ready(function() {
 		$("#field").keyup(function(e) {
 			if(e.keyCode == 13) {
@@ -39,5 +39,5 @@
 			}
 		});
 	});
- 
+
 }
